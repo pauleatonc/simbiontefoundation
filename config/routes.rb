380c54bc56e6f3f9bd22ee049b9acdbb33fde437
Paul Eaton <pauleatonc@gmail.com>
2021-04-08 15:46:16 -0400

    comenting devise on routes and pushing acording to stackoverflow

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'landing#index'
 
-  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+  # devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
   get 'landing/about'
   get 'landing/contact'

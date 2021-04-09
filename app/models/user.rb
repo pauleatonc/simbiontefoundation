735a89435a98dff79c2acc61a2e252f9720567b4
Paul Eaton <pauleatonc@gmail.com>
2021-04-09 15:18:48 -0400

    working on omniauth in heroku 7

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :omniauthable, omniauth_providers: [:google_oauth2]
+         
 end

c1598a887f99b8a1e11a571431a3a0549b61e3a0
Paul Eaton <pauleatonc@gmail.com>
2021-04-28 01:15:54 -0400

    switching script to improve performance

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -1,13 +1,17 @@
 class LandingController < ApplicationController
   def index
+    @user = current_user
     end
 
   def about
+    @user = current_user
     end
 
   def contact
+    @user = current_user
     end
 
   def dashboard
+    @user = current_user
     end
   end
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,12 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
+require("packs/chart")
+require("packs/gustav")
+require("packs/jquery-3.2.1")
+require("packs/jquery.cookie-1.4.1")
+require("packs/karma.min.js")
+
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
